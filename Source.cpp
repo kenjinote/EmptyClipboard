bd09f615422a34b67435f6238835a3e8d6fff70a
kenjinote <2605401+kenjinote@users.noreply.github.com>
2023-02-04 02:44:51 +0900

    プロジェクト ファイルを追加します。

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+	if (OpenClipboard(0)) {
+		EmptyClipboard();
+		CloseClipboard();
+	}
+	return 0;
+}
